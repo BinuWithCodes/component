cd16f8d793d0f752c49e3e263279440cc48763f2
BinuWithCodes <chat2binu@gmail.com>
2022-02-06 19:58:45 +0530

    first component react

---
+++ src/Form.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor(props) {
+      super(props)
+    this.state = {
+         name: '',
+         Comments: ''
+       
+      }
+    }
+
+    UsernameChange = event => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    
+    CommentsChange = event => {
+        this.setState({
+            Comments: event.target.value
+        })
+    }
+    
+    Submit = event => {
+        document.write(`${this.state.username} ${this.state.Comments} `)
+        event.preventDefault()
+    }
+
+
+  render() {
+    return (
+        <form onSubmit={this.Submit}>
+          
+            <div>
+                <label>Comments</label>
+                <input value={this.state.Comments} 
+                onChange={this.CommentsChange}></input>
+            </div>
+          
+            <button type='submit'>Submit</button>
+        </form>
+    )
+  }
+}
+
+export default Form;
